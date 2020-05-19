74e6a3e1c7937c6ae468dce8ee93f6a2aa32e7bb
katopodis@mail.ru <katopodis@mail.ru>
2020-05-19 10:55:37 +0400

    flash message bug fix

--- resources/views/admin/partials/includes/css.blade.php
+++ resources/views/admin/partials/includes/css.blade.php
@@ -3,7 +3,7 @@
 <!-- Font Awesome CSS-->
 <link rel="stylesheet" href="{{asset('vendor/font-awesome/css/font-awesome.min.css')}}">
 <!-- Fontastic Custom icon font-->
-<link rel="stylesheet" href="{{asset('css/fontastic.css')}}">
+<link rel="stylesheet" href="{{asset('css/css/fontastic.css')}}">
 <!-- Google fonts - Roboto -->
 <link rel="stylesheet" href="{{asset('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700')}}">
 <!-- Custom Scrollbar-->
@@ -13,15 +13,14 @@
 <!-- Custom stylesheet - for your changes-->
 <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
 <!-- Favicon-->
-<link rel="shortcut icon" href="{{asset('img/favicon.ico')}}">
+{{--<link rel="shortcut icon" href="{{asset('img/favicon.ico')}}">--}}
 
-<link rel="stylesheet" type="text/css" href="{{asset('css/select2.min.css')}}" />
+<link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}" />
+{{--<link rel="stylesheet" type="text/css" href="{{asset('css/select2.min.css')}}" />--}}
 
-<link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}" />
 <link rel="stylesheet" type="text/css" href="{{asset('css/rubik.css')}}" />
 <link rel="stylesheet" type="text/css" href="{{asset('css/fontawesome.min.css')}}" />
 <link rel="stylesheet" type="text/css" href="{{asset('css/font-awesome.min.css')}}" />
-<link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}" />
 {{--css star--}}
 <link rel="stylesheet" type="text/css" href="{{asset('css//css/all.css')}}" />
 <link rel="stylesheet" type="text/css" href="{{asset('css//css/all.min.css')}}" />
@@ -36,4 +35,5 @@
 <link rel="stylesheet" type="text/css" href="{{asset('css//css/v4-shims.css')}}" />
 <link rel="stylesheet" type="text/css" href="{{asset('css//css/v4-shims.min.css')}}" />
 
+<link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}" />
 @stack('push_css')
--- resources/views/admin/partials/includes/top_js.blade.php
+++ resources/views/admin/partials/includes/top_js.blade.php
@@ -1,4 +1,2 @@
-{{--<script type="text/javascript" src="{{asset('js/jquery-3.2.1.slim.min.js')}}"></script>--}}
 <script type="text/javascript" src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
-
 @stack('top_js')
